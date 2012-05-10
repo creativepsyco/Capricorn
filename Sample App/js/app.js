6d82844268e2589b26733b0fcc5c235c1984df6e
Mohit Kanwal <mohit.kanwal@gmail.com>
2012-05-10 17:01:50 +0800

    SampleApp
    Complete Working Condition

---
+++ Sample App/js/app.js
@@ -0,0 +1,67 @@
+window.HomeView = Backbone.View.extend({
+
+    template:_.template($('#home').html()),
+
+    render:function (eventName) {
+        $(this.el).html(this.template());
+        return this;
+    }
+});
+
+window.Page1View = Backbone.View.extend({
+
+    template:_.template($('#page1').html()),
+
+    render:function (eventName) {
+        $(this.el).html(this.template());
+        return this;
+    }
+});
+
+// Routes
+var AppRouter = Backbone.Router.extend({
+
+    routes:{
+        "":"home",
+        "page1":"page1",
+    },
+
+    initialize:function () {
+        // Handle back button throughout the application
+        $('.back').live('click', function(event) {
+            window.history.back();
+            return false;
+        });
+        this.firstPage = true;
+    },
+
+    home:function () {
+        console.log('#home');
+        this.changePage(new HomeView());
+    },
+
+    page1:function () {
+        console.log('#page1');
+        this.changePage(new Page1View());
+    },
+
+    changePage:function (page) {
+        $(page.el).attr('data-role', 'page');
+        page.render();
+        $('body').append($(page.el));
+        var transition = $.mobile.defaultPageTransition;
+        // We don't want to slide the first page
+        if (this.firstPage) {
+            transition = 'none';
+            this.firstPage = false;
+        }
+        $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
+    }
+
+});
+
+$(document).ready(function () {
+    console.log('document ready');
+    app = new AppRouter();
+    Backbone.history.start();
+});
