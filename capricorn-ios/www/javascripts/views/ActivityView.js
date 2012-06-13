b172c2ef714107e848f6facc0eca2e70ed6b4679
Amulya Khare <amulyakhare@gmail.com>
2012-06-14 03:49:15 +0800

    added icons for activity filter

--- capricorn-ios/www/javascripts/views/ActivityView.js
+++ capricorn-ios/www/javascripts/views/ActivityView.js
@@ -10,6 +10,9 @@ window.ActivityView = Backbone.View.extend({
     resetBtnStates: function() {
         $('#all-btn div').css('font-weight','normal');
         $('#all-btn div').css('text-decoration','underline');
+        $('#qn-btn img').attr('src','css/images/questionIcon-grey-24X24.png');
+        $('#ans-btn img').attr('src','css/images/answerIcon-grey-24X24.png');
+        $('#cmnt-btn img').attr('src','css/images/comment-Blue-24X24.png');
     },
 
     onShowAllClick: function() {
@@ -22,6 +25,7 @@ window.ActivityView = Backbone.View.extend({
 
     onQuestionClick: function() {
         this.resetBtnStates();
+        $('#qn-btn img').attr('src','css/images/questionIcon-colour-24X24.png');
         this.questionList = new ActivityList(this.activityList.getQuestions());
         $(this.el).find('#activity-lst').html(new ActivityListView({model:this.questionList}).render().el);
         setTimeout(this.refreshScroller(), 200);
@@ -29,6 +33,7 @@ window.ActivityView = Backbone.View.extend({
 
     onAnswerClick: function() {
         this.resetBtnStates();
+        $('#ans-btn img').attr('src','css/images/answerIcon-colour-24X24.png');
         this.answeList = new ActivityList(this.activityList.getAnswers());
         $(this.el).find('#activity-lst').html(new ActivityListView({model:this.answeList}).render().el);
         setTimeout(this.refreshScroller(), 200);
@@ -36,6 +41,7 @@ window.ActivityView = Backbone.View.extend({
 
     onCommentClick: function() {
         this.resetBtnStates();
+        $('#cmnt-btn img').attr('src','css/images/comment-Grey-24X24.png');
         this.commentList = new ActivityList(this.activityList.getComments());
         $(this.el).find('#activity-lst').html(new ActivityListView({model:this.commentList}).render().el);
         setTimeout(this.refreshScroller(), 200);
