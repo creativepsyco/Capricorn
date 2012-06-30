746670a8593ffcb699b0138057f3ac74fe980f71
Mohit Kanwal <mohit.kanwal@gmail.com>
2012-07-01 01:58:25 +0900

    Reusing only one model

--- capricorn-ios/www/javascripts/models/settings.js
+++ capricorn-ios/www/javascripts/models/settings.js
@@ -2,25 +2,6 @@ window.SettingsModel = Backbone.Model.extend({});
 
 window.AttachmentModel = Backbone.Model.extend({});
 
-
-window.QuestionDelete = Backbone.Model.extend({
-	url: function() {
-		return "http://pakora.herokuapp.com/question/delete/" + this.qid + "/" + this.uid;
-	}
-});
-
-window.AnswerDelete = Backbone.Model.extend({
-	url: function() {
-		return "http://pakora.herokuapp.com/answer/delete/" + this.aid + "/" + this.uid;
-	}
-});
-
-window.AnswerCommentDelete = Backbone.Model.extend({
-	url: function() {
-		return "http://pakora.herokuapp.com/answercomment/delete/" + this.cid + "/" + this.uid;
-	}
-});
-
 window.ModelDelete = Backbone.Model.extend({
 	url: function() {
 		return "http://pakora.herokuapp.com/" + this.type + "/delete/" + this.id + "/" + this.uid;
